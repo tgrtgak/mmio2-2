3e5cee5cb0375d1eb2a2de2674b7bfc4831d5817
wilkie <wilkie05@gmail.com>
2020-09-14 00:51:29 -0400

    Fixes binutils build for latest emscripten.
    I think!!

--- scripts/build-toolchain.sh
+++ scripts/build-toolchain.sh
@@ -4,25 +4,30 @@ cd riscv-gnu-toolchain
 if [ ! -f riscv-binutils/bfd/doc/chew ]; then
   cp -r riscv-binutils riscv-binutils-js
   cd riscv-binutils
-  ./configure --prefix $PWD/../../utils --target=riscv64-unknown-elf
+  ./configure --prefix $PWD/../../utils --target=riscv64-unknown-elf --disable-nls
   make
   make install
   cd ..
 fi
 
+PROFILING_OPTS=
+
+# Uncomment to gain stack traces
+#PROFILING_OPTS=-g --profiling-funcs 
+
 cd riscv-binutils-js
-emconfigure ./configure --target=riscv64-unknown-elf
-emmake make all-ld CFLAGS="-DHAVE_PSIGNAL -m32"
+emconfigure ./configure --target=riscv64-unknown-elf --disable-nls
+emmake make all-ld CFLAGS="-DHAVE_PSIGNAL -m32 ${PROFILING_OPTS} -s ALLOW_MEMORY_GROWTH=1"
 cp ../riscv-binutils/bfd/doc/chew bfd/doc/chew
 chmod +x bfd/doc/chew
 cp ../riscv-binutils/binutils/sysinfo binutils/sysinfo
 chmod +x binutils/sysinfo
-emmake make all-ld CFLAGS="-DHAVE_PSIGNAL -m32"
+emmake make all-ld CFLAGS="-DHAVE_PSIGNAL -m32 ${PROFILING_OPTS} -s ALLOW_MEMORY_GROWTH=1"
 cp ../riscv-binutils/bfd/doc/chew bfd/doc/chew
 chmod +x bfd/doc/chew
 cp ../riscv-binutils/binutils/sysinfo binutils/sysinfo
 chmod +x binutils/sysinfo
-emmake make all-ld CFLAGS="-DHAVE_PSIGNAL -m32"
+emmake make all-ld CFLAGS="-DHAVE_PSIGNAL -m32 ${PROFILING_OPTS} -s ALLOW_MEMORY_GROWTH=1"
 cd ../..
 
 rm -f riscv-gnu-toolchain/riscv-binutils-js/libiberty/getopt*.o
@@ -34,10 +39,18 @@ rm -f riscv-gnu-toolchain/riscv-binutils-js/ld/*testplug*.o
 # This gets a _memset not defined in the emscripten implementation of the get_rusage syscall.
 # There is no mechanism for emitting the _memset function in emscripten. Needs to be a
 # js dep, but I can't figure out how to do that.
-emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/gas -name \*.o` -o assets/js/riscv64-unknown-elf-as-bare.js -s WASM=1 -s ASSERTIONS=1 -lworkerfs.js -lfs.js
-emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` `ls riscv-gnu-toolchain/riscv-binutils-js/ld/*.o` -o assets/js/riscv64-unknown-elf-ld-bare.js -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s "BINARYEN_METHOD='native-wasm'" -s "EXPORTED_FUNCTIONS=['_memset']" -s NO_EXIT_RUNTIME=0 -lworkerfs.js -s ASSERTIONS=1
-emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` riscv-gnu-toolchain/riscv-binutils-js/binutils/objdump.o riscv-gnu-toolchain/riscv-binutils-js/binutils/{bucomm,dwarf,prdbg,debug,elfcomm,version,rddbg,filemode,stabs,rdcoff}.o -o assets/js/riscv64-unknown-elf-objdump-bare.js -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s "BINARYEN_METHOD='native-wasm'" -s "EXPORTED_FUNCTIONS=['_memset']" -s NO_EXIT_RUNTIME=0 -lworkerfs.js -s ASSERTIONS=1
-emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` riscv-gnu-toolchain/riscv-binutils-js/binutils/readelf.o riscv-gnu-toolchain/riscv-binutils-js/binutils/{unwind-ia64,dwarf,prdbg,debug,elfcomm,version,rddbg,filemode,stabs,rdcoff}.o -o assets/js/riscv64-unknown-elf-readelf-bare.js -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s "BINARYEN_METHOD='native-wasm'" -s NO_EXIT_RUNTIME=0 -lworkerfs.js -s ASSERTIONS=1
+
+# Assembler (as)
+emcc -v -O2 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/gas -name \*.o` -o assets/js/riscv64-unknown-elf-as-bare.js -s WASM=1 -s ASSERTIONS=1 -lworkerfs.js -lfs.js -s NO_EXIT_RUNTIME=0 -s ALLOW_MEMORY_GROWTH=1 ${PROFILING_OPTS}
+
+# Linker (ld)
+emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` `ls riscv-gnu-toolchain/riscv-binutils-js/ld/*.o` -o assets/js/riscv64-unknown-elf-ld-bare.js -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s ASSERTIONS=1 -lworkerfs.js -lfs.js ${PROFILING_OPTS}
+
+# Disassembler (objdump)
+emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` riscv-gnu-toolchain/riscv-binutils-js/binutils/objdump.o riscv-gnu-toolchain/riscv-binutils-js/binutils/{bucomm,dwarf,prdbg,debug,elfcomm,version,rddbg,filemode,stabs,rdcoff}.o -o assets/js/riscv64-unknown-elf-objdump-bare.js -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -lworkerfs.js -s ASSERTIONS=1 -lworkerfs.js -lfs.js ${PROFILING_OPTS}
+
+# Metadata-reader (readelf)
+emcc -v -O3 `find riscv-gnu-toolchain/riscv-binutils-js/intl -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/zlib -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/opcodes -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libiberty -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/libctf -name \*.o` `find riscv-gnu-toolchain/riscv-binutils-js/bfd -name \*.o` riscv-gnu-toolchain/riscv-binutils-js/binutils/readelf.o riscv-gnu-toolchain/riscv-binutils-js/binutils/{unwind-ia64,dwarf,prdbg,debug,elfcomm,version,rddbg,filemode,stabs,rdcoff}.o -o assets/js/riscv64-unknown-elf-readelf-bare.js -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -lworkerfs.js -lfs.js -s ASSERTIONS=1 ${PROFILING_OPTS}
 
 # Prepend and append the shims
 cat lib/pre-worker.js assets/js/riscv64-unknown-elf-as-bare.js lib/post-worker.js > assets/js/riscv64-unknown-elf-as.js
