9025084840eb59043d06aeb7a4925aba8a7ab2d1
wilkie <wilkie05@gmail.com>
2020-09-21 19:03:48 -0400

    Adds selenium karma support.

--- karma.conf.js
+++ karma.conf.js
@@ -9,11 +9,15 @@ module.exports = function(config) {
     // We want inline source maps with jasmine/karma
     webpackConfig.devtool = "inline-source-map";
 
+    let browsers = ["ChromeHeadless",  "ChromiumHeadless"];
+    if (process.env["SELENIUM_URL"]) {
+        browsers = ["Chrome_selenium"];
+    }
     config.set({
         // These might be useful to run without the origin checking:
         //browsers: ["ChromeHeadless", "Chrome_without_security", "ChromiumHeadless", "Chromium_without_security"],
 
-        browsers: ["ChromeHeadless",  "ChromiumHeadless"],
+        browsers: browsers,
 
         customLaunchers: {
             Chrome_without_security: {
@@ -23,6 +27,16 @@ module.exports = function(config) {
             Chromium_without_security: {
                 base: "ChromiumHeadless",
                 flags: ['--disable-web-security']
+            },
+            Chrome_selenium: {
+                base: 'SeleniumWebdriver',
+                browserName: 'Chrome',
+                getDriver: function() {
+                    return new webdriver.Builder()
+                        .forBrowser('chrome')
+                        .usingServer(process.env["SELENIUM_URL"] || 'http://localhost:4444/wd/hub')
+                        .build()
+                }
             }
         },
 
