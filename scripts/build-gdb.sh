9561c68da0ccc7d831303add5538dc6a1a8f9262
wilkie <wilkie05@gmail.com>
2021-02-22 00:27:19 -0500

    Adds patch to remove get_tty_state warnings in gdb.

--- scripts/build-gdb.sh
+++ scripts/build-gdb.sh
@@ -34,6 +34,7 @@ patch -N -p1 < ../../patches/gdb-emscripten.patch
 patch -N -p1 < ../../patches/gdb-emscripten-readline.patch
 patch -N -p1 < ../../patches/gdb-emscripten-fix-maint.patch
 patch -N -p1 < ../../patches/gdb-emscripten-remote-jsinvoke.patch
+patch -N -p1 < ../../patches/gdb-emscripten-get_tty_state.patch
 
 # Configure
 echo ""
