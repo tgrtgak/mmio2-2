15f3412d8b1f807a25407928f5522fe504335aef
wilkie <wilkie05@gmail.com>
2021-02-23 15:58:02 -0500

    Updates electron build script to properly build the release.
    Adds the app icon, but does not package it yet. That's a little more
    complicated as it requires updating the resource within the executable.

--- electron/build-win.sh
+++ electron/build-win.sh
@@ -1,10 +1,33 @@
-wget https://github.com/electron/electron/releases/download/v11.3.0/electron-v11.3.0-win32-x64.zip
+#!/bin/bash
+
+echo "Downloading Electron"
+echo "===================="
+
+wget -nc "https://github.com/electron/electron/releases/download/v11.3.0/electron-v11.3.0-win32-x64.zip"
+
+echo "Copying"
+echo "======="
+
+if [ ! -d ../static/public ]; then
+  echo "Error: No static site found."
+  echo "       Use \`package.sh\` in the root directory to create."
+  exit
+fi
+
 mkdir -p builds/windows
 cd builds/windows
-unzip ../../electron*win32-x64.zip
+unzip -u ../../electron*win32-x64.zip
+mv electron.exe rawrs.exe
+mkdir -p resources/app
 cp -r ../../../static/public resources/app/.
 cp ../../main.js resources/app/.
+cp ../../package.json resources/app/.
 cd ../..
 
+echo ""
+echo "Packaging"
+echo "========="
+
 mkdir -p releases
-zip builds/windows/* releases/rawrs-win32-x64.zip
+cd builds/windows
+zip -r ../../releases/rawrs-win32-x64.zip *
