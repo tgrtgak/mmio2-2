ad9538bf02632b7b918cbf02f06dd3b9bd093857
Patrick Stoyer <stoyerp8991@gmail.com>
2021-03-16 12:42:08 -0400

    formatting change

--- kernel/paging.s
+++ kernel/paging.s
@@ -25,9 +25,9 @@ paging_expand_heap:
   push s1
   push s2
 
-  bltz a0,_expand_heap_failure  #if n < 0, return (should we cause exception?)
-  lw   s0, heap_address         #retain original heap address to return later
-  move s1, a0                   #number of bytes to expand by
+  bltz a0,_expand_heap_failure  # if n < 0, return (should we cause exception?)
+  lw   s0, heap_address         # retain original heap address to return later
+  move s1, a0                   # number of bytes to expand by
   li   t0, 0x10040000 
   lw   t1, heap_address 
   bne  t0, t1, _expand_heap_not_first_page 
@@ -43,22 +43,22 @@ _expand_heap_not_first_page:
 _expand_heap_continue:
   move s2, a0
   
-  jal paging_get_root   #returns root page table address in a0
-  print_hex a0
-  move a1, s2           #physical address to start at (return of paging_v_to_p or memory_alloc_page) 
-  print_hex a1
-  lw   a2, heap_address #virtual address to map to 
-  print_hex a2
-  move a3, s1           #number of bytes to add
-  print_hex a3
-  li   a4,  PTE_USER | PTE_WRITE| PTE_READ| PTE_EXECUTE    #flags;
-  print_hex a4
+  jal paging_get_root   # returns root page table address in a0
+  #print_hex a0
+  move a1, s2           # physical address to start at (return of paging_v_to_p or memory_alloc_page) 
+ # print_hex a1
+  lw   a2, heap_address # virtual address to map to 
+ # print_hex a2
+  move a3, s1           # number of bytes to add
+ # print_hex a3
+  li   a4,  PTE_USER | PTE_WRITE| PTE_READ| PTE_EXECUTE    # flags;
+ # print_hex a4
   jal paging_map_range
-  lw   a0, heap_address #increment heap_address by n
+  lw   a0, heap_address # increment heap_address by n
   add  t0, a0, s1
   la   t1, heap_address
   sw   t0, (t1)
-  move a0, s0           #move original heap address to a0 to return
+  move a0, s0           # move original heap address to a0 to return
   j _expand_heap_exit
 _expand_heap_failure:
   li a0, -1
